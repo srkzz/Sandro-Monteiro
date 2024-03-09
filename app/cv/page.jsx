b298df51825ea92d3bc205a60904fb9f1e143637
Ruben Alexandre Silva <66267648+srkzz@users.noreply.github.com>
2024-03-09 16:53:32 +0000

    Update page.jsx

--- app/cv/page.jsx
+++ app/cv/page.jsx
@@ -7,18 +7,18 @@ const CV = () => {
         <div className='mt-10 mb-10 w-full max-w-2x1 flex flex-col gap-7 glassmorphism'>
             <div className='flex justify-center items-center'>
                 <header className="random_gradient text-center head_text">
-                    <h1 className="font-bold">Ruben Silva</h1>
+                    <h1 className="font-bold">Sandro Monteiro</h1>
 
-                    <Image src="/assets/images/eu.png" width={150}
+                    <Image src="/assets/images/sandro.jpg" width={150}
                         height={150} className="rounded-full" alt="profile" />
-                    <h2 className="text-lg font-semibold">Junior Developer</h2>
+                    <h2 className="text-lg font-semibold">O tolo</h2>
                 </header>
             </div>
             <section className="my-3 mx-4 glassmorphism">
                 <h3 className="text-xl font-semibold mb-4">Contact Information</h3>
-                <p>Mobile: +351 925992819</p>
-                <p>Email: ruben_alex_23@hotmail.com</p>
-                <p>LinkedIn: <a href="www.linkedin.com/in/ruben-silva-a96866272">www.linkedin.com/in/ruben-silva-a96866272</a></p>
+                <p>Mobile: 911 945 500</p>
+                <p>Email: sandro.rafael.sr7s@gmail.com</p>
+                <p>LinkedIn: <a href="www.linkedin.com/">www.linkedin.com/</a></p>
                 <p>GitHub: <a href="github.com/srkzz">github.com/srkzz</a></p>
             </section>
 
@@ -35,9 +35,9 @@ const CV = () => {
             <section className="my-3 mx-4 glassmorphism">
                 <h3 className="text-xl font-semibold mb-4">Certifications</h3>
                 <ul>
-                    <li>Commercial English (IEFP, January 2021 - August 2021)</li>
-                    <li>TVDE Driving</li>
-                    <li>Programmer Level 4 - EISNT + IEFP </li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
                 </ul>
             </section>
 
@@ -45,11 +45,11 @@ const CV = () => {
                 <h3 className="text-xl font-semibold mb-4">Programming Professional Experience</h3>
                 <article className="mb-6">
                     <h4 className="text-lg font-semibold">Junior Developer</h4>
-                    <p>Develop2You</p>
+                    <p></p>
                     <p>Porto, Portugal</p>
-                    <p>March 2023 - April 2023 (2 months)</p>
+                    <p></p>
                     <ul>
-                        <li>Developed a full-stack web project using php, Laravel, mysql.</li>
+                        <li></li>
                     </ul>
                 </article>
 
@@ -60,9 +60,9 @@ const CV = () => {
             <section className="my-3 mx-4 glassmorphism">
                 <h3 className="text-xl font-semibold mb-4">Education</h3>
                 <article className="mb-6">
-                    <h4 className="text-lg font-semibold">Developer, Computer Programming/Programmer, General Computing</h4>
-                    <p>Eisnt - Centro de Formação Profissional</p>
-                    <p>February 2022 - May 2023</p>
+                    <h4 className="text-lg font-semibold"></h4>
+                    <p></p>
+                    <p></p>
                 </article>
 
                 {/* Other education entries */}
