85e2cc7836eefad9cb6fa8dfaeec9a36383a921e
Ruben Alexandre Silva <66267648+srkzz@users.noreply.github.com>
2024-03-13 21:02:56 +0000

    Create page.jsx

---
+++ app/modelacao/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Slider from '@components/Slider';
+
+
+export default function modelacao(){
+    return (
+        
+        <div className="mb-8">
+             <Slider />
+        </div>
+           
+       
+    );
+};
