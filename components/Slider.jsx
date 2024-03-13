72e0d88a19781effa8bba9933b18af669e7b61c7
Ruben Alexandre Silva <66267648+srkzz@users.noreply.github.com>
2024-03-13 21:07:54 +0000

    Update Slider.jsx

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -2,10 +2,22 @@ import React from 'react';
 import Carousel from './Carousel';
 
 const slides = [
-  "./assets/images/Diamante.PNG",
-  "./assets/images/Cadeira.png",
-  "./assets/images/Tabuleiro.png",
-  "./assets/images/Torre_ v1.png",
+  "./assets/images/jante.PNG",
+  "./assets/images/Peca1.PNG",
+  "./assets/images/Peca2.png",
+  "./assets/images/alfinete.png",
+  "./assets/images/audi.png",
+   "./assets/images/balljoint.png",
+   "./assets/images/bispo.png",
+   "./assets/images/bolabasket.png",
+   "./assets/images/cadeira.png",
+   "./assets/images/clip.png",
+   "./assets/images/ferramenta1.png",
+   "./assets/images/pecacomfuros.png",
+    "./assets/images/pokebola.png",
+    "./assets/images/tabuleiro.png",
+    "./assets/images/torre.png",
+    "./assets/images/sandro.png",
 ];
 
 
